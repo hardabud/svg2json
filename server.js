da22ee9ab3195d21413198fec3018cef8094acde
anders <anders@hardabud.com>
2015-04-16 19:43:50 +0200

    cleanup

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ var fs = require('fs');
 app.set('views', './views');
 app.set('view engine', 'jade');
 
-app.use('/js', express.static(__dirname + '/client/js'));
-app.use('/css', express.static(__dirname + '/client/css'));
-
+app.use('/js', express.static(__dirname + '/public/js'));
 
 app.get('/', function(req, res) {
 	var jsonFiles = fs.readdirSync('./json');
@@ -19,15 +17,5 @@ app.get('/:file', function(req, res) {
 	var file = require('./json/' + fileName);
 	res.render('image', {file: file})
 })
-
-app.get('/img/', function(req, res) {
-	var jsonFiles = fs.readdirSync('./json');
-	res.send(jsonFiles)
-});
-app.get('/img/:file', function(req, res) {
-	var fileName = req.params.file;
-	var file = require('./json/' + fileName);
-	res.send(file)
-});
 
 app.listen(3000, function() { console.log('listening on 3000')})
